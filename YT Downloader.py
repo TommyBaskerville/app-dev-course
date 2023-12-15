cfcfd9ef3ef622ca71c0383233269bb158cb44be
TommyBaskerville <147432909+TommyBaskerville@users.noreply.github.com>
2023-12-14 22:38:27 -0600

    Logo,text,link input

--- YT Downloader.py
+++ YT Downloader.py
@@ -9,12 +9,23 @@ from kivy.uix.image import Image
 from kivy.core.window import Window
 
 Window.size = (500,600)
-Window.clearcolor = (1,0,1,1)
+Window.clearcolor = (1,1,1,1)
 
 class MyApp(App):
     def build(self):
         layout = RelativeLayout()
         
+        self.image = Image(source="logo.svg", size_hint=(.5, .5), pos_hint={"center_x":.5, "center_y":.90})
+        
+        self.youtubelink = Label(text="Youtube Link", size_hint=(1,1), pos_hint={"center_x":.5, "center_y":.75}, font_size=20, color=(0,0,0,1))
+        
+        self.linkinput = TextInput(size_hint=(.8,.05), pos_hint={"center_x":.5, "center_y":.65}, multiline=False)
+        
+        layout.add_widget(self.linkinput)
+        
+        layout.add_widget(self.youtubelink)
+        
+        layout.add_widget(self.image)
         return layout
     
 if __name__ == "__main__":
